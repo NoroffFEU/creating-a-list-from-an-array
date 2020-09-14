b9690a0aa842a6847e22896ae20c960a423695ca
cnnrbrn <connorbobrien@gmail.com>
2020-09-14 21:08:24 +0200

    Add delete buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const listItems = [];
+let listItems = [];
 
 const listInput = document.querySelector("input");
 const button = document.querySelector("button");
@@ -12,6 +12,7 @@ function addToList() {
         listItems.push(newItem);
         createList();
         listInput.value = "";
+        listInput.focus();
         console.log(listItems);
     }
 }
@@ -22,7 +23,7 @@ function createList() {
     listContainer.innerHTML = "";
 
     listItems.forEach(function (item) {
-        listContainer.innerHTML += `<li><span>${item}</span></li>`;
+        listContainer.innerHTML += `<li><span>${item}</span> <i class="fa fa-trash" data-item="${item}"></li</li>`;
     });
 
     const items = document.querySelectorAll("li span");
@@ -30,8 +31,30 @@ function createList() {
     items.forEach(function (item) {
         item.addEventListener("click", handleClick);
     });
+
+    const trashCans = document.querySelectorAll("li i");
+
+    trashCans.forEach(function (can) {
+        can.addEventListener("click", removeFromList);
+    });
 }
 
 function handleClick() {
     event.target.classList.toggle("complete");
+}
+
+function removeFromList() {
+    console.log(event);
+
+    const deleteThisItem = event.target.dataset.item;
+
+    const newList = listItems.filter(function (item) {
+        if (deleteThisItem !== item) {
+            return true;
+        }
+    });
+
+    listItems = newList;
+
+    createList();
 }
