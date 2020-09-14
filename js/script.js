ab46ee10fd8aac763ef393d3938f8d7c5578f29b
cnnrbrn <connorbobrien@gmail.com>
2020-09-14 20:40:39 +0200

    Add first files

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const listItems = [];
+
+const listInput = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", addToList);
+
+function addToList() {
+    const newItem = listInput.value.trim();
+
+    if (newItem.length >= 2) {
+        listItems.push(newItem);
+        createList();
+        listInput.value = "";
+        console.log(listItems);
+    }
+}
+
+function createList() {
+    const listContainer = document.querySelector("ul");
+
+    listContainer.innerHTML = "";
+
+    listItems.forEach(function (item) {
+        listContainer.innerHTML += `<li><span>${item}</span></li>`;
+    });
+
+    const items = document.querySelectorAll("li span");
+
+    items.forEach(function (item) {
+        item.addEventListener("click", handleClick);
+    });
+}
+
+function handleClick() {
+    event.target.classList.toggle("complete");
+}
